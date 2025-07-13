e4e995ed10eb85fead208f7c7e7ff19010f02fcf
Nijhan <najmashaaban6@gmail.com>
2025-07-13 14:51:17 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// src/TestComponent.js
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const TestComponent = () => {
+  return (
+    <div>
+      <video title="time video" controls>
+        <source src="sample.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default TestComponent;
